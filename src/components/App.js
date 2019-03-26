603b72d17030b0b5b92fd0574f7b07f5c1d17cf2
Daniel Laurindo <dslaurindo@gmail.com>
2019-03-26 15:41:51 -0300

    Add a menu in the header

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,23 @@ import Card from './Card/Card';
 
 const App = () => (
     <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="#">Navbar</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/quiz">Quiz</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
         <Card items={[1,2,3,4,5]} />
-        <Link to="/quiz">Ir para a página sobre \o/</Link>
     </div>
 );
 
