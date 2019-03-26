74466089f7900b8e4cbd2b21bbb9713e0bf126ad
Daniel Laurindo <dslaurindo@gmail.com>
2019-03-26 15:02:10 -0300

    Add webpack to dependencies to work in heroku deployment

--- index.js
+++ index.js
@@ -11,5 +11,6 @@ app.get('/',function(req,res) {
 
 app.use(express.static(__dirname + '/dist'));
 
-app.listen(process.env.port || 3000);
-console.log('Running at Port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
+console.log(`Running at Port ${PORT}`);
