2651ad2c57aa8402f33485c7ace4ca2a1cb181ed
Daniel Laurindo <dslaurindo@gmail.com>
2019-03-26 14:45:24 -0300

    Add webpack to dependencies to work in heroku deployment

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-	entry: './src/main.js',
+	entry: __dirname + '/src/main.js',
 	output: {
 		path: __dirname + '/dist',
 		filename: 'bundle.js'
