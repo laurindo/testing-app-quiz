a357b2dd64175ef6469244ce36aa54d993ee50aa
Daniel Laurindo <dslaurindo@gmail.com>
2019-03-26 11:18:25 -0300

    Preparing server to run in heroku production

---
+++ index.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const fs = require('fs');
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.get('/',function(req,res) {
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
+
+app.use(express.static(__dirname + '/dist'));
+
+app.listen(process.env.port || 3000);
+console.log('Running at Port 3000');
